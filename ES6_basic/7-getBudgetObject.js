fe551a9dfa3fef1489f8dc936bb4ebf388927dcf
adoniskoco <10028@holbertonstudents.com>
2025-03-27 14:33:37 +0100

    Added task 7

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,5 +1,7 @@
 /*eslint-disable*/
+// Function that returns an object with shorthand properties
 export default function getBudgetObject(income, gdp, capita) {
+	// Using shorthand object property names
 	const budget = {
 	  income,
 	  gdp,
@@ -8,3 +10,4 @@ export default function getBudgetObject(income, gdp, capita) {
   
 	return budget;
   }
+  
